90bb57819300a606adbc071147bc06cf71ab713a
daeho <daehocho0637@gmail.com>
2024-12-01 21:54:44 +0900

    문제 지문 추가

--- 241130_array_average.playground/Contents.swift
+++ 241130_array_average.playground/Contents.swift
@@ -1,6 +1,9 @@
 import UIKit
 
-/// 나의 풀이
+// 나의 풀이
+/// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+/// - Parameter numbers: 정수 배열
+/// - Returns: 배열의 평균
 func solution(_ numbers:[Int]) -> Double {
     var sum = 0.0
     numbers.forEach { number in
