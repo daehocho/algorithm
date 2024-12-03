b505e057d946b0ad2c2098524580ec702dca0814
daeho <daehocho0637@gmail.com>
2024-12-03 17:52:03 +0900

    - 문제 지문 추가

--- 241203_element_lengths.playground/Contents.swift
+++ 241203_element_lengths.playground/Contents.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 // 나의 풀이
+/// 문자열 배열 strlist가 매개변수로 주어집니다. strlist 각 원소의 길이를 담은 배열을 return하도록 solution 함수를 완성해주세요.
 func solution(_ strlist:[String]) -> [Int] {
     var result: [Int] = []
     strlist.forEach { text in
