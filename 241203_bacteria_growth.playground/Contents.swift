b339c50a329abb77b16050ee02962ec7df061370
daeho <daehocho0637@gmail.com>
2024-12-04 17:39:03 +0900

    - Lv.0_세균 증식
    프로그래머스 알고리즘 문제

---
+++ 241203_bacteria_growth.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+/// 어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+func solution(_ n:Int, _ t:Int) -> Int {
+    var bacteria = n
+    
+    for i in 0..<t {
+        bacteria = (bacteria * 2)
+    }
+    return bacteria
+}
+
+print(solution(2, 10))
+print(solution(7, 15))
